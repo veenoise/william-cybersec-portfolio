3f1aeadde5496b710e68d5d8628d5b59bc473554
veenoise <william.eduard123@gmail.com>
2024-11-15 14:38:28 +0800

    Add: experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,28 @@ function Experience() {
 			<div className="container" style={{marginTop: "56px"}} id="experience">
 				<h2 className="mb-4">experience</h2>
 				<div>
+					<h5>Cybersecurity Trainee (Mar 2024 - Present)</h5>
+					<h6 className="mb-3">TryHackMe</h6>
+					<div>
+						<ul>
+							<li>Practical experience in cybersecurity tools such as NMAP, Burp Suite, Metasploit, OWASP ZAP, Snort, Splunk, and more</li>
+							<li>Ranked top 5% in the platform</li>
+							<li>200+ days streak on continuous learning</li>
+							<li>3000+ events in the yer 2024</li>
+							<li>80+ completed rooms</li>
+							<li>13+ badges obtained such as Linux PrivEsc, Hash Cracker, Cyber Ready, and Blue</li>
+						</ul>
+					</div>
+					<h5>Web Application Security Trainee (Aug 2024 - Present)</h5>
+					<h6 className="mb-3">PortSwigger</h6>
+					<div>
+						<ul>
+							<li>Utilized the knowledge to perform web application security testing in a university-wide social media application</li>
+							<li>Practical experience in Burp Suite Community edition</li>
+							<li>Employ safe web application development practices such as applying CSRF token and double submit cookie with JWT tokens</li>
+							<li>Implement secure web applications by using standards in backend comunication with databases such as using question mark placeholders</li>
+						</ul>
+					</div>
 					<h5>Cybersecurity Lead (Sep 2023 - Jul 2024)</h5>
 					<h6 className="mb-3">Google Developer Student Club - PUP</h6>
 					<div>
@@ -12,16 +34,6 @@ function Experience() {
 							<li>Session speaker for bash scripting</li>
 							<li>Conduct 6 online cybersecurity sessions</li>
 							<li>Manage over 50+ cybersecurity cadets</li>
-						</ul>
-					</div>
-					<h5>UI/UX Virtual Apprentice (Sep 2023)</h5>
-					<h6 className="mb-3">MedGrocer - KadaKareer</h6>
-					<div>
-						<ul>
-							<li>Created UI/UX for MedGrocer</li>
-							<li>Collaborated with team members</li>
-							<li>Participate in weekly meetings</li>
-							<li>Received special mention from KadaKareer</li>
 						</ul>
 					</div>
 					<h5>IT Intern (Aug - Sep 2023)</h5>
