afa003acf18d61407f5b6accb13b646a8ccd5afe
veenoise <william.eduard123@gmail.com>
2024-11-23 19:56:07 +0800

    Add routing and new skills

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,6 +1,11 @@
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="container" style={{height: "calc(100vh - 56px)", marginTop: "56px", overflow: "hidden"}}>
+      <div className="d-flex justify-content-center mt-3">
+        
+      </div>
+      
+    </section>
   )
 }
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,8 @@
+import { Link, useLocation } from 'react-router-dom';
+
 function Nav(){
+	const location = useLocation();
+
 	return (
 		<>
 			<nav className="navbar navbar-dark navbar-expand-md fixed-top">
@@ -12,27 +16,47 @@ function Nav(){
 						</div>
 						<div className="offcanvas-body">
 							<ul className="navbar-nav justify-content-end justify-content-md-center flex-grow-1 gap-md-3">
+								{
+									location.pathname === '/' ? 
+									<>
+										<li className="nav-item">
+											<a className="nav-link text-center" data-to-scrollspy-id="home" href="#home">Home</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-center" data-to-scrollspy-id="skills" href="#skills">Skills</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-center" data-to-scrollspy-id="experience" href="#experience">Experience</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-center" data-to-scrollspy-id="projects" href="#projects">Projects</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-center" data-to-scrollspy-id="contact" href="#contact">Contact</a>
+										</li>
+									</>
+									:
+									<li className='nav-item'>
+										<Link to="/" style={{textDecoration: "none"}} className="nav-link text-center">
+											Home
+										</Link>
+									</li>
+									
+								}
+								
 								<li className="nav-item">
-									<a className="nav-link text-center" data-to-scrollspy-id="home" href="#home">Home</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link text-center" data-to-scrollspy-id="skills" href="#skills">Skills</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link text-center" data-to-scrollspy-id="experience" href="#experience">Experience</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link text-center" data-to-scrollspy-id="projects" href="#projects">Projects</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link text-center" data-to-scrollspy-id="contact" href="#contact">Contact</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link text-center" href="" style={{
-										background:"-webkit-linear-gradient(200deg, hsla(0, 0%, 6%, 1) 5%, hsla(348, 83%, 47%, 1) 100%)", 
-										borderRadius:"10px", 
-										fontWeight: "bold"
-									}}>Blogs</a>
+									<Link 
+										to="/blogs" 
+										className="nav-link text-center"
+										style={{
+											textDecoration: "none", 
+											background:"-webkit-linear-gradient(200deg, hsla(0, 0%, 6%, 1) 5%, hsla(348, 83%, 47%, 1) 100%)", 
+											borderRadius:"10px", 
+											fontWeight: "bold"
+										}}>
+											Blogs
+									</Link>
+									
 								</li>
 							</ul>
 						</div>
