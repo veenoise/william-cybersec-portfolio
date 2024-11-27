026176c3b8c8bad0af5f63bd41a55b939a7da101
veenoise <william.eduard123@gmail.com>
2024-11-27 09:28:35 +0800

    Update: changed error-page.jsx to redirect to proper hashRoute

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,6 +1,22 @@
-import { useRouteError } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
+  const homeEndpoints = [
+    '/home', 
+    '/skills', 
+    '/experience', 
+    '/projects', 
+    '/contact'
+  ];
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {  
+    if (homeEndpoints.includes(location.pathname)) {
+      navigate(`/#/${location.pathname.substring(1)}`)
+    }
+    
+  }, [])
   const error = useRouteError();
 
   return (
@@ -9,11 +25,18 @@ export default function ErrorPage() {
       className="d-flex flex-column align-items-center justify-content-center" 
       style={{height: "100vh"}}
     >
-      <h1 className="text-center">Oops!</h1>
-      <p className="text-center">Sorry, an unexpected error has occurred.</p>
-      <p className="text-center">
-        <i className="text-center">{error.statusText || error.message}</i>
-      </p>
+      {
+        !homeEndpoints.includes(location.pathname) ?
+        <>
+          <h1 className="text-center">Oops!</h1>
+          <p className="text-center">Sorry, an unexpected error has occurred.</p>
+          <p className="text-center">
+            <i className="text-center">{error.statusText || error.message}</i>
+          </p> 
+        </>:
+        <></>
+      }
+      
     </div>
   );
 }
